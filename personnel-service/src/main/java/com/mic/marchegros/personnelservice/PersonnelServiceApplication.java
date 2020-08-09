6dac09d5e4b7f45132826820a0c3609a15799f87
ynsl31 <ynsln1998@gmail.com>
2020-08-09 21:12:15 +0100

    init

---
+++ personnel-service/src/main/java/com/mic/marchegros/personnelservice/PersonnelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mic.marchegros.personnelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelServiceApplication.class, args);
+	}
+
+}
