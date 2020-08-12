8474cc515d76635f318fbdc9eaa06eee9ef1bb53
Youness LAAOUANE <61345973+ynsl31@users.noreply.github.com>
2020-08-12 22:48:32 +0100

    init service hangar

---
+++ sic.mg.hangar/src/test/java/ma/sic/mg/hangar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.sic.mg.hangar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
